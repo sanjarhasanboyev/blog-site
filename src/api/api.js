065a03cb2a286e5b3bcdb5cbdd670969c37c4fa8
Sanjar Hasanboyev <sanjarkhasanov11@gmail.com>
2026-04-10 15:05:09 +0500

    url changed at 10.04.2026

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://gale-appreciated-reconstruction-blast.trycloudflare.com',
+  baseURL: 'https://highway-illustration-sought-released.trycloudflare.com',
   headers: {
     'Content-Type': 'application/json',
   },
